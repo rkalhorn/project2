e0d19c34eb8ae06cc9083b9310ba87c36d2fd901
RKalhorn <rkalhorn@gmail.com>
2018-10-29 19:11:56 -0500

    final?

--- app.py
+++ app.py
@@ -23,9 +23,13 @@ mongo = PyMongo(app)
 
 results_csv = 'db/results.csv'
 stlctycombined_csv = 'db/stlctycombined.csv'
+nbhdInfo_csv = 'db/Neighborhood_Info.csv'
+nbhdZip_csv = 'db/Neighborhood_zip.csv'
  
 results_data = []
 stlctycombined_data = []
+nbhdInfo_data = []
+nbhdZip_data = []
  
 with open(results_csv, "r") as f:
     reader = csv.DictReader(f)
@@ -42,14 +46,40 @@ with open(results_csv, "r") as f:
 with open(stlctycombined_csv, "r") as f:
     reader = csv.DictReader(f)
 
-    # Iterate through each zip...
+    # Iterate through each entry...
     for row in reader:
         temp = {}
-        # Iterate through each field of target zip
+        # Iterate through each field of target entry
         for field, val in row.items():
             temp[field] = val
-        # Append zip to our data list
+        # Append entry to our data list
         stlctycombined_data.append(temp)
+
+with open(nbhdInfo_csv, "r") as f:
+    reader = csv.DictReader(f)
+
+    # Iterate through each neighborhood...
+    for row in reader:
+        temp = {}
+        # Iterate through each field of neighborhood entry
+        for field, val in row.items():
+            temp[field] = val
+        # Append neighborhood to our data list
+        nbhdInfo_data.append(temp)
+
+
+with open(nbhdZip_csv, "r") as f:
+    reader = csv.DictReader(f)
+
+    # Iterate through each neighborhood...
+    for row in reader:
+        temp = {}
+        # Iterate through each field of neighborhood entry
+        for field, val in row.items():
+            temp[field] = val
+        # Append neighborhood to our data list
+        nbhdZip_data.append(temp)
+
 
 
 client = MongoClient('localhost', 27017)
@@ -60,6 +90,11 @@ collection1.insert(results_data)
 collection2 = db['stlctycombined']
 collection2.insert(stlctycombined_data)
 
+collection3 = db['nbhdInfo']
+collection3.insert(nbhdInfo_data)
+
+# collection4 = db['nbhdZip']
+# collection4.insert(nbhdZip_data)
 # with open('stlresults.json', 'w') as outfile:
 #     json.dump(results_data, outfile)
 
@@ -97,12 +132,32 @@ def zip_data(zip):
 
     output = []
     
-    output.append({'zip' : this_zip['Zip'], 'realRating' : this_zip['RelRating'], 
+    output.append({'zip' : this_zip['Zip'], 'lat' : this_zip['lat'], 'lng' : this_zip['lng'], 
+    'realRating' : this_zip['RelRating'], 
     'busRating' : this_zip['BusRating'], 'edRating' : this_zip['EdRating'],
     'crimeRating': this_zip['CrimeRating'], 'popRating' : this_zip['PopRating'],
     'incomeRating': this_zip['IncomeRating']})
     return jsonify({'result' : output})
 
+@app.route("/zip_nbhd_data/<zip>")
+def zip_nbhd_data(zip):  
+    myzip = 'zip'+zip
+    print(myzip)
+    nbhds = mongo.db.nbhdInfo.find({myzip : 'TRUE'})
+    print(nbhds.count())
+    output = [] 
+
+    for doc in nbhds:
+        output.append({'neighborhood' : doc['Neighborhood'], 'pop(census2010)' : doc['Population (2010 Census)'], 
+            # 'white' : doc['White'], 'black' : doc['Black'], 
+            # 'hispanic/latino': doc['Hispanic/Latino (May be of any race)'],
+            # 'indian/alaskan' : doc[' American Indian or Alaskan Native'], 'asian': doc['Asian'], 
+            # 'mixed' : doc['Mixed Race'], 
+            'nbhdProfile' : doc['Neighborhood Profile Link'], 
+            'lat': doc['lat'], 'lng': doc['lng'], 
+            'wiki' : doc['Wikipedia Link'], 'author' : doc['You need to attribute the author']})
+    return jsonify({'result' : output})
+ 
 
 @app.route("/zip_map_data/<zip>")
 def zip_map_data(zip):
--- static/js/app_personal_zip.js
+++ static/js/app_personal_zip.js
@@ -2,7 +2,9 @@
 // Creating map object
 var map = L.map("map", {
     center: [38.6270, -90.25],
-    zoom: 12
+    zoom: 12,
+    zoomDelta: 0.25,
+    zoomSnap: .1
 });
 
 
@@ -33,9 +35,6 @@ function resize(){
 }
 map.invalidateSize();
 
-// function buildMap(zip){
-
-// }
 
 function getZipInfo(zip){
     //Get location on page to put data
@@ -59,42 +58,30 @@ function getZipInfo(zip){
         document.getElementById('real_rating').innerHTML = myData['realRating'];
         //document.getElementById('business_rating').innerHTML =  ;
         
-        mykeys = d3.keys(myData);
+        map.panTo(new L.LatLng(myData['lat'], myData['lng']));
+        map.setZoom(12.5);
 
-        myvalues = d3.values(myData);
-        console.log(myvalues);
-        var myobjects = mykeys.map(function(e, i) {
-            var returnVal = e + " : "+ myvalues[i] + "</br>";
-            console.log(returnVal);
-            return returnVal;
-        });
+ 
        
-        // ul.selectAll('li')
-        //     .data(myobjects)
-        //     .enter()
-        //     .append('li')
-        //     .html(String); 
+         
       });
 }
 
 function addMarkersToMap(zip){
-
+    
     map.eachLayer(function (layer) {
         map.removeLayer(layer);
     });
     tile.addTo(map);
     var zipURL = `/zip_map_data/${zip}`;
-    console.log(zipURL);
-
 
     
     d3.json(zipURL, function(data){ 
         myData = data.result;
 
-        
-
         var busMarkers = [];
         var edMarkers = [];
+        var relMarkers = [];
 
         for (var i = 0; i < myData.length; i++) {
           // loop through the data, create a new marker, push it to the cityMarkers array
@@ -105,90 +92,135 @@ function addMarkersToMap(zip){
           );
           }
           else if(myData[i].type=="R"){
-            edMarkers.push(
+            relMarkers.push(
                 L.circle([myData[i].lat,myData[i].lng], {color: 'black', weight: '1', fillColor: 'green', fillOpacity: 0.5, radius: 200})
                 .bindPopup("<p>" + myData[i].name + "</p>")
             );
             }
           else if(myData[i].type=="B"){
               busMarkers.push(
-            L.marker([myData[i].lat,myData[i].lng]).bindPopup("<p>" + myData[i].name + "</p>")
+                L.circle([myData[i].lat,myData[i].lng], {color: 'black', weight: '1', fillColor: 'blue', fillOpacity: 0.5, radius: 200})
+                .bindPopup("<p>" + myData[i].name + "</p>")
+            
             );
               }
         }
         L.layerGroup(busMarkers).addTo(map);
         L.layerGroup(edMarkers).addTo(map);
-
-
-
-        // myData.forEach(function(point){
-        //     var lat = point.lat;
-        //     var lon = point.lng;
-        //     var feature = {type: 'Feature',
-        //         properties: point,
-        //         geometry: {
-        //             type: 'Point',
-        //             coordinates: [lon,lat]
-        //         }
-        //     };
-        //     if(point.type == "B"){bjsonFeatures.push(feature);}
-        //     else if(point.type == "C"){cjsonFeatures.push(feature);}
-        //     else if(point.type == "E"){ejsonFeatures.push(feature);}
-        //     else if(point.type == "R"){rjsonFeatures.push(feature);}
-        // });
-        //  console.log(bjsonFeatures);
-        // var bgeoJson = { type: 'FeatureCollection', features: bjsonFeatures };
-        // //var cgeoJson = { type: 'FeatureCollection', features: cjsonFeatures };
-        // var egeoJson = { type: 'FeatureCollection', features: ejsonFeatures };
-        // var rgeoJson = { type: 'FeatureCollection', features: rjsonFeatures };
-        
-        // var bgeojsonMarkerOptions = {
-        //     radius: 8,
-        //     fillColor: "#ff7800",
-        //     color: "#000",
-        //     weight: 1,
-        //     opacity: 1,
-        //     fillOpacity: 0.8
-        // };
-        
-        
-
-        // L.geoJSON(bgeoJson, {
-        //     pointToLayer: function (feature, latlng) {
-        //         return L.marker(latlng, bgeojsonMarkerOptions);
-        //     }
-        // }).addTo(map);
-
-
-
-
-
-
-
-        // myData = data.result[0];
-        // document.getElementById('business_rating').innerHTML = myData['busRating'];
-        // document.getElementById('education_rating').innerHTML = myData['edRating'];
-        // document.getElementById('crime_rating').innerHTML = myData['crimeRating'];
-        // document.getElementById('real_rating').innerHTML = myData['realRating'];
-        // //document.getElementById('business_rating').innerHTML =  ;
-        
-        // mykeys = d3.keys(myData);
-
-        // myvalues = d3.values(myData);
-        // console.log(myvalues);
-        // var myobjects = mykeys.map(function(e, i) {
-        //     var returnVal = e + " : "+ myvalues[i] + "</br>";
-        //     console.log(returnVal);
-        //     return returnVal;
+        L.layerGroup(relMarkers).addTo(map);
         });
+        
+        
+        
+        var zipURL2 = `/zip_nbhd_data/${zip}`;
+        var nbhdMarkers = [];
+    d3.json(zipURL2, function(data){
+        
+        myData = data.result;
+        
+        
+        var nbhds = [];
+        for (var i = 0; i < myData.length; i++){
+            var mylat = myData[i]['lat'];
+            var mylng = myData[i]['lng'];
+            var mynbhd = myData[i]['neighborhood'];
+
+console.log(mylng);
+            if(nbhds.indexOf(mynbhd)<0){
+
+                nbhds.push(mynbhd);
+
+                nbhdMarkers.push(
+                    L.marker([mylat,-mylng]).bindPopup("<p>" + mynbhd + "</p>")
+                    );
+                
+            }
+            
+        }
+        L.layerGroup(nbhdMarkers).addTo(map);
+         
+    });
+    
 }
 
+function addNbhdTable(zip){
+    var zipURL = `/zip_nbhd_data/${zip}`;
+    document.getElementById("mytable").innerHTML = "";
+   
+    //create table
+    var table = document.createElement('table');
+    document.getElementById("mytable").appendChild(table);
+    
+    var nbhds = [];
+
+    var cols = ['Neighborhood', 'Population (Census 2010)', 'Wikipedia Page for Neighborhood'];
+    // Add the header row.
+    var header = table.createTHead();
+    var row = header.insertRow(-1);
+    for (var i = 0; i < cols.length; i++) {
+        var headerCell = document.createElement('th');
+        headerCell.innerText = cols[i]; 
+        row.appendChild(headerCell);
+    }
+    // Create table body.
+ 
+    var tBody = document.createElement('tbody');
+ 
+    table.appendChild(tBody);
+
+     // Add the data rows to the table body.
+ 
+    d3.json(zipURL, function(data){
+        
+        myData = data.result;
+        for (var i = 0; i < myData.length; i++){
+            var nbhd = myData[i]['neighborhood'];
+            var pop = myData[i]['pop(census2010)'];
+            var wiki = myData[i]['wiki'];
+            var mylink = myData[i]['nbhdProfile'];
+
+            if(nbhds.indexOf(nbhd)<0){
+
+                nbhds.push(nbhd);
+                row = tBody.insertRow(-1);
+     
+                
+                var cell1 = row.insertCell(-1);
+                cell1.setAttribute('data-label', cols[0]);
+                var cellLink = document.createElement("a");
+                cellLink.setAttribute("href", mylink);
+                cellLink.innerHTML= nbhd;
+                cell1.appendChild(cellLink);
+
+                
+                //var mylink = document.createElement("a");
+                // cell1.setAttribute('href',link);
+                // cell1.innerText = nbhd;    
+
+                var cell2 = row.insertCell(-1);
+                cell2.setAttribute('data-label', cols[1]);
+                cell2.innerText = pop;    
+
+                var cell3 = row.insertCell(-1);
+                cell3.setAttribute('data-label', cols[2]);
+                cell3.innerText = wiki; 
+
+
+        
+            }
+            
+        }
+         
+    });
+         
+}
 
 function optionChanged(newZip) {
     //Fetch new data each time a new sample is selected
     //buildMap(newZip);
     getZipInfo(newZip);
     addMarkersToMap(newZip);
+    addNbhdTable(newZip);
 }
 
   
@@ -197,66 +229,6 @@ function optionChanged(newZip) {
 
 
 
-
-// function chooseColor(borough){
-//     switch(borough){
-//         case "Brooklyn":
-//             return "green";
-//             break;
-//         case "Bronx":
-//             return "yellow";
-//             break;
-//         case "Staten Island":
-//             return "red";
-//             break;
-//         case "Manhattan":
-//             return "blue";
-//             break;
-//         case "Queens":
-//             return "purple";
-//             break;
-//         default:
-//             return "grey";
-        
-//     }
-// }
-
-
-// //D3 geoJson request
-// d3.json(link, function(dataset){
-//     L.geoJSON(dataset,{
-//         style: function(feature){
-//             return{
-//                 color: "white",
-//                 fillColor: chooseColor(feature.properties.borough),
-//                 fillOpacity: 0.5,
-//                 weight: 1.9
-//             };
-//         },
-//         onEachFeature: function(feature, layer){
-//             layer.on({
-//                 mouseover: function(event){
-//                     layer = event.target;
-//                     layer.setStyle({
-//                         fillOpacity: .75
-//                     });
-//                 },
-//                 mouseout: function(event){
-//                     layer = event.target;
-//                     layer.setStyle({
-//                         fillOpacity: .5
-//                     });
-//                 },
-//                 click: function(event){
-//                     map.fitBounds(event.target.getBounds());
-//                 },
-//             })
-//             layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1>" +
-//                             "<h2>" + feature.properties.borough + "</h2>"
-//             );
-//         }
-//     }).addTo(map);
-// });
 
 
 
